2d65935b2c1af01d38089fd9f044327e92aeb17d
Unknown <lorenz.hufe@web.de>
2017-06-26 13:12:45 +0200

    Including Board

---
+++ Board.py
@@ -0,0 +1,20 @@
+
+
+class Board():
+
+    board = None
+
+    def Board(self):
+        self.board = []
+
+        board.append( Rock("A1",True) )
+        board.append( Knight("B1",True) )
+        board.append( Bishop("C1",True) )
+        board.append( Queen("D1", True) )
+        board.append( King("E1", True) )
+        board.append( Bishop("F1", True) )
+        board.append( Knight("G1", True) )
+        board.append( Rock("H1", True) )
+
+    def toString(self):
+        return None
