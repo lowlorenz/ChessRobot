c3ec8d5c67f809d1079a0157bc71f7509d6b7e32
Unknown <lorenz.hufe@web.de>
2017-06-26 13:12:32 +0200

    Including Figures

---
+++ Figure.py
@@ -0,0 +1,11 @@
+class Figure():
+
+    def __init__(self, position):
+        self.position = (str)(position)
+        pass
+
+    def move(self):
+        pass
+
+    def toString(self):
+        return "X"
---
+++ Pawn.py
@@ -0,0 +1,13 @@
+from Figure import Figure
+
+class Pawn(Figure):
+
+    def __init__(self, position, white):
+        Figure.__init__(self,position)
+        if( white ):
+            self.white = True
+        else:
+            self.white = False
+
+    def __str__(self):
+        return "P"    
